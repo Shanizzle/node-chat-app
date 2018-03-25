ca3e43047a4fcd824824277db477ed3fa0e2406a
SHANIZZLE <shannenreese@gmail.com>
2018-03-25 14:29:17 -0500

    Add form for messages and show incoming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -10,6 +10,22 @@ socket.on('disconnect', function() {
   console.log('Disconnected from server');
 });
 
-socket.on('newMessage', function(createMessage) {
-  console.log('New Message', createMessage);
+socket.on('newMessage', function(message) {
+  console.log('newMessage', message);
+  let li = $('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+
+  $('#messages').append(li);
+});
+
+
+$('#message-form').on('submit', function(e) {
+  e.preventDefault();
+
+  socket.emit('createMessage',  {
+    from: 'User',
+    text: $('#message').val()
+  }, function() {
+
+  });
 });
